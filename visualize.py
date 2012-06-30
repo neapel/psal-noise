1d46c4e772b70d2ff02f77874da9c5251ec26d76
Pascal Schmitt <pascal.schmitt1@stud.uni-goettingen.de>
2012-06-30 04:29:37 +0200

    viz

--- visualize.py
+++ visualize.py
@@ -6,9 +6,14 @@ from glob import glob
 import sys
 import os.path
 from numpy import *
+from random import random
+from operator import itemgetter
 
 ALL = 160
 ELITE = 20
+
+def jitter(d = 2):
+	return (random() - 0.5) / d
 
 def read_one_pop(f, limit):
 	gen = []
@@ -36,22 +41,29 @@ def read_last_pop(name, limit=ELITE):
 			if x:
 				return x
 
+def read_this_pop(name, number, limit=ELITE):
+	with open('{0}/{1:05d}.pop'.format(name, number)) as f:
+		x = read_one_pop(f, limit)
+		if x:
+			return x
+
 def read_gen(name):
 	for fname in sorted(glob(name + '/*.gen')):
 		with open(fname) as f:
 			gen = {}
 			for line in f:
 				fields = line.split()
-				if fields[0] == 'elite':
-					gen[fields[1]] = (fields[1],)
-				elif fields[0] == 'mate' and fields[3] == 'children':
-					gen[fields[4]] = (fields[1], fields[2])
-					gen[fields[5]] = (fields[1], fields[2])
+				if len(fields) > 1:
+					if fields[0] == 'elite':
+						gen[fields[1]] = (fields[1],)
+					elif fields[0] == 'mate' and fields[3] == 'children':
+						if fields[4] not in gen:
+							gen[fields[4]] = (fields[1], fields[2])
+						if fields[5] not in gen:
+							gen[fields[5]] = (fields[1], fields[2])
 			if len(gen) > 0:
 				yield gen
 
-def mean(points, at=1):
-	return sum([p[at] for p in points]) / len(points)
 
 def smooth(points, window=1+2*25):
 	if window == 0:
@@ -62,39 +74,45 @@ def smooth(points, window=1+2*25):
 	return out[window:-window]
 
 def overview(name):
-	d = list(read_pop(name))
-	md = map(mean, d)
-	smd = smooth(md)
+	pop = array(map(lambda x: map(itemgetter(1), x), read_pop(name, ALL)))
+	elite = pop[:,:ELITE]
+	elite_mean = list(mean(elite, 1))
+	smooth_mean = smooth(elite_mean, 1+2*5)
 	print 'reset'
-	print 'set title "%s"' % (name,)
+	print 'set title "{0}"'.format(name)
 	print 'unset key'
-	print 'set style line 1 pt 5 ps 0.01 lw 2 lc rgbcolor "red"'
+	print 'set style line 1 pt 5 ps 0.2 lw 2 lc rgbcolor "red"'
 	print 'set style line 2 lc rgbcolor "#606060"'
-	print 'set style line 3 pt 5 ps 0.05 lc rgbcolor "black"'
+	print 'set style line 3 pt 5 ps 0.1 lc rgbcolor "black"'
+	print 'set style line 4 pt 5 ps 0.05 lc rgbcolor "gray"'
 	print 'set xtics border nomirror out 50 offset 0,0.25 font "Sans, 8"'
 	print 'set mxtics 5'
 	print 'unset ytics'
-	print 'set border 1 ls 2'
+	print 'set border 0 ls 2'
 	print 'set rmargin 5'
 	print 'set xrange [0:*]; set yrange [0:*]'
-	print 'set label 1 "%d" at %f,%f nopoint offset 0.25,0 textcolor ls 1 font "Sans Bold, 10"' % (smd[-1], len(smd), smd[-1])
-	print 'plot "-" volatile using ( column(-1) + 0.5 * (rand(0) - 0.5)):1 w p ls 3, ',
+	print 'set label 1 "{0:.0f}" at {1},{0} '.format(smooth_mean[-1], len(smooth_mean)),
+	print '  nopoint offset 0.25,0 textcolor ls 1 font "Sans Bold, 10"'
+	print 'plot ',
+	print '"-" volatile w p ls 4, ',
+	print '"-" volatile w p ls 3, ',
 	print '"-" volatile w p ls 1, ',
 	print '"-" volatile w l ls 1'
-	for p in d:
-		for i in p: print i[1]
-		print
+	for i, gen in enumerate(pop[:,ELITE:]): # normal
+		for x in gen: print i + jitter(), x
 	print 'e'
-	for p in md: print p
+	for i, gen in enumerate(elite): # elite
+		for x in gen: print i + jitter(), x
 	print 'e'
-	for p in smd: print p
+	for p in elite_mean: print p
+	print 'e'
+	for p in smooth_mean: print p
 	print 'e'
 
 def overviews(*names):
 	print 'set terminal pngcairo notransparent size 1000,400'
 	for name in names:
-		print 'print "{0}"'.format(name)
-		print 'set output "{0}.png"'.format(name)
+		print 'print "{0}" ; set output "{0}.png"'.format(name)
 		overview(name)
 
 def averages(*names):
@@ -113,6 +131,8 @@ def averages(*names):
 			i/len(names), os.path.basename(name))
 		for i, name in enumerate(names)
 	])
+	def mean(points, at=1):
+		return sum([p[at] for p in points]) / len(points)
 	for name in names:
 		m = map(mean, read_pop(name))
 		for x in m: print x
@@ -153,18 +173,30 @@ def one_pop(name):
 	for x in pop_a: print x
 	print 'e'
 
-def profile(name):
+def profile(ptype, name):
 	profiles = []
 	for g in read_pop(name):
-		profile = array([0.0] * 32)
+		p = []
 		for i in g:
-			profile += array(map(int, i[0]))
-		profile /= len(g)
-		profiles.append(profile)
+			p.append(array(map(int, i[0])))
+		p = vstack(p)
+		if ptype == 'mean':
+			profiles.append(mean(p, 0))
+		else:
+			profiles.append(var(p, 0))
 	profiles = vstack(profiles)
 	print 'reset'
-	print 'set palette rgb 33,13,10'
-	print 'unset colorbox'
+	print 'set palette rgb 31,13,10'
+
+	print 'set rmargin 10'
+	if ptype == 'mean':
+		print 'set palette rgb 9,13,-9'
+		print 'set cbtics scale 0 ("{0}" 0, "{1}" 1)'.format(*map(binstring, '01'))
+	else:
+		print 'set palette rgb -7,-7,-7'
+		print 'set cbtics scale 0 ("0" 0, "0.25" 0.25)'
+	h = 0.125
+	print 'set colorbox user noborder origin graph 1.01, graph {0} size character 2, graph {1}'.format((1-h)/2, h)
 	print 'set yrange [-0.5:31.5]'
 	print 'set lmargin 8'
 	print 'unset border'
@@ -179,13 +211,10 @@ def profile(name):
 	print 'e'
 	print 'e'
 
-
-
-
 def lambda_hist(*names):
 	lambdas = [0] * 32
 	for name in names:
-		for indiv in read_last_pop(name, 10):
+		for indiv in read_this_pop(name, 200, 10):
 			lambdas[indiv[0].count('1')] += 1
 	paper_lambdas = [ 0,  0,  0,  0,  0,  0,  0,  0,
 	                  0,  0,  2, 16, 21, 37, 39, 31,
@@ -207,6 +236,116 @@ def lambda_hist(*names):
 	print 'e'
 	for x in paper_lambdas: print x
 	print 'e'
+
+def parenthood(name): # run112
+	n = 7
+	pop = list({} for _ in range(n + 1))
+	for i, g in zip(range(n + 1), read_pop(name, ALL)):
+		for j, r in enumerate(g):
+			pop[i][r[0]] = (i + jitter(8), r[1], j)
+
+	super_elite_lines = []
+	super_mate_lines = []
+	elite_lines = []
+	mate_lines = []
+	for i, g in zip(range(n), read_gen(name)):
+		for k, v in g.items():
+			child = pop[i + 1].get(k, None)
+			if child:
+				parents = map(lambda x: pop[i][x], v)
+				appto = None
+				if child[2] == 0:
+					appto = super_elite_lines if len(parents) == 1 else super_mate_lines
+				else:
+					appto = elite_lines if len(parents) == 1 else mate_lines
+				for p in parents:
+					appto.append( child + p )
+
+	print 'reset'
+	print 'set key left top samplen 1'
+	print 'unset border'
+	print 'unset ytics'
+	print 'set xtics scale 0 font ",8"'
+	print 'set xrange [-0.5:{0}]'.format(n + 0.5)
+	print 'set style line 1 lw 0.1 lc rgbcolor "blue"'
+	print 'set style line 2 lw 0.5 lc rgbcolor "red"'
+	print 'plot ',
+	print '"-" w l ls 1 notitle,',
+	print '"-" w l ls 2 notitle,',
+	print '"-" w l ls 1 lw 1.25 t "Crossover",',
+	print '"-" w l ls 2 lw 1.5 t "Kopiert",',
+	print '"-" w p pt 5 ps 0.3 lc rgbcolor "black" t "Regeln" '
+	def lines(l):
+		for x1, y1, _, x2, y2, _ in l:
+			print x1, y1, '\n', x2, y2, '\n'
+		print 'e'
+	lines(mate_lines)
+	lines(elite_lines)
+	lines(super_mate_lines)
+	lines(super_elite_lines)
+	for gen in pop:
+		for x, y, _ in gen.values(): print x, y
+	print 'e'
+
+
+def genealogy(name):
+	n = 150
+	pop = list({} for _ in range(n + 1))
+	elite_threshold = []
+	poor_threshold = []
+	for i, g in zip(range(n + 1), read_pop(name, ALL)):
+		for j, r in enumerate(g):
+			pop[i][r[0]] = (i + jitter(), r[1], j)
+		elite_threshold.append(g[ELITE - 1][1])
+		poor_threshold.append(g[ALL//2][1])
+
+	old_elite = []
+	new_elite = []
+	rest = []
+	for i, g in zip(range(n), read_gen(name)):
+		for k, v in g.items():
+			child = pop[i + 1].get(k, None)
+			if child:
+				if len(v) == 1:
+					old_elite.append(child)
+				elif child[2] < ELITE:
+					new_elite.append(child)
+				else:
+					rest.append(child)
+					#for p in v:
+					#	parent = pop.get((i, p), None)
+					#	if parent:
+					#		lines.append( parent + child )
+
+	rest = rest + new_elite
+	new_elite = []
+
+	print 'reset'
+	print 'set key left top samplen 1'
+	print 'unset border'
+	print 'unset ytics'
+	print 'set xtics scale 0 font ",8"'
+	print 'set xrange [0.51:{0}]'.format(n + 0.49)
+	print 'plot ',
+	print '"-" w p pt 5 ps 0.05 lc rgbcolor "#808080" t "Normale", ',
+	print '"-" w histeps lc rgbcolor "black" t "Elitegrenze", ',
+	print '"-" w histeps lc rgbcolor "black" t "Median", ',
+	print '"-" w p pt 5 ps 0.3 lc rgbcolor "red" t "Alte Elite", ',
+	print '"-" w p pt 5 ps 0.2 lc rgbcolor "blue" t "Neu in Elite" '
+	for x, y, _ in rest: print x, y
+	print 'e'
+	for y in elite_threshold: print y
+	print 'e'
+	for y in poor_threshold: print y
+	print 'e'
+	for x, y, _ in old_elite: print x, y
+	print 'e'
+	for x, y, _ in new_elite: print x, y
+	print 'e'
+
+
+	
+
 
 if __name__ == '__main__':
 	names = list([os.path.splitext(n)[0] for n in sys.argv[2:]])
